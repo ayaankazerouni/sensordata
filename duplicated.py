64196c3dc5abbd784a9fc09784828b6814fc2a3f
Ayaan Kazerouni <ayaan@vt.edu>
2016-07-18 15:28:30 -0400

    Gets duplicates from time spent.

--- duplicated.py
+++ duplicated.py
@@ -25,15 +25,16 @@ def get_duplicated(infile, outfile):
         # Set initial values
         prev_row = None
 
-        # Cache will have structure like this:
+        # Store will have structure like this:
         # {
         #     userId: {
         #         projectId: {
-        #             import event with timestamp
+        #             [ list of timestamps of import events ]
         #         }
         #     }
         # }
-        cache = {}
+        store = {}
+        written = {}
 
         for row in reader:
             if repr(row['Subtype']) == repr('Import'):
@@ -43,35 +44,68 @@ def get_duplicated(infile, outfile):
                 subtype = row['Subtype']
                 uri = row['uri']
 
-                if user_id in cache:
-                    if project_id in cache[user_id]:
-                        if time in cache[user_id][project_id]:
-                            writer.writerow({ 'userId': user_id, 'projectId': project_id, 'time': time, 'uri': uri, \
-                                'subtype': subtype })
-                        else:
-                            cache[user_id][project_id].append(time)
-                    else:
-                        times = [ time ]
-                        cache[user_id][project_id] = times
+                if time not in store.get(user_id, {}).get(project_id, []):
+                    store.setdefault(user_id, {}).setdefault(project_id, []).append(time)
                 else:
-                    cache[user_id] = {}
-                    cache[user_id][project_id] = [ time ]
+                    if time not in written.get(user_id, {}).get(project_id, []):
+                        writer.writerow({ 'userId': user_id, 'projectId': project_id, 'time': time, 'uri': uri, \
+                            'subtype': subtype })
+                        written.setdefault(user_id, {}).setdefault(project_id, []).append(time)
+
+def get_duplicated_from_time_spent(infile, outfile):
+    """
+    Prints out the userId and projectId for projects
+    that appear twice with the same data in a time_spent.csv
+    file.
+    """
+    print('Getting duplicates from the time_spent file...')
+    fieldnames = [ 'userId', 'projectId' ]
+
+    with open(infile, 'r') as fin, open(outfile, 'w') as fout:
+        reader = csv.DictReader(fin, delimiter=',')
+        writer = csv.DictWriter(fout, delimiter=',', fieldnames=fieldnames)
+
+        # Write headers to outfile.
+        writer.writerow(dict((fn, fn) for fn in writer.fieldnames))
+
+        prev_row = None
+        start = True
+        written = {}
+
+        for row in reader:
+            user_id = row['userId']
+            project_id = row['projectId']
+
+            if start:
+                prev_row = row
+                start = False
+            elif row == prev_row and project_id not in written.get(user_id, []):
+                writer.writerow({ 'userId': user_id, 'projectId': project_id })
+                written.setdefault(user_id, []).append(project_id)
+
+            prev_row = row
 
 def main(args):
-    infile = args[0]
-    outfile = args[1]
+    method = args[0]
+    infile = args[1]
+    outfile = args[2]
     try:
-        get_duplicated(infile, outfile)
+        if method == 'raw':
+            get_duplicated(infile, outfile)
+        elif method == 'time':
+            get_duplicated_from_time(infile, outfile)
+        else:
+            print_usage()
     except FileNotFoundError as e:
         print("Error! File '%s' does not exist." % infile)
 
 def print_usage():
     print("Get userId, projectId, uri, time, and Subtype for projects where " +
         " an Import event occurs at the same timestamp.")
-    print("Usage: ./duplicated.py <input_file> <output_file>")
+    print("Usage:\n./duplicated.py <raw | time> <input_file> <output_file>")
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 4:
         print_usage()
         sys.exit()
     main(sys.argv[1:])
