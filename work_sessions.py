b01b09f0a5be24d8ca91bea7695a014d64afa071
Ayaan Kazerouni <ayaan@vt.edu>
2016-09-08 16:31:21 -0400

    Work sessions show green zones per ws.

--- work_sessions.py
+++ work_sessions.py
@@ -10,7 +10,7 @@ def get_work_sessions(infile, outfile):
     fieldnames = ['projectId', 'userId', 'cleaned_assignment', 'milestone1', 'milestone2', 'milestone3',
         'earlyBonus', 'dueTime', 'workSessionId', 'start_time', 'end_time',\
         'normalLaunches', 'testLaunches', 'editSizeStmts', 'testEditSizeStmts', 'editSizeMethods',\
-        'testEditSizeMethods']
+        'testEditSizeMethods', 'greenZones']
 
     prev_row = None
     ws = None
@@ -21,6 +21,7 @@ def get_work_sessions(infile, outfile):
     test_edit_size_stmts = 0
     edit_size_methods = 0
     test_edit_size_methods = 0
+    green_zones = 0
 
     with open(infile, 'r') as fin, open(outfile, 'w') as fout:
         reader = csv.DictReader(fin, delimiter=',')
@@ -39,8 +40,14 @@ def get_work_sessions(infile, outfile):
                     edit_size_methods += int(row['editSizeMethods'])
                     test_edit_size_methods += int(row['testEditSizeMethods'])
 
+                    successes = int(row['successes'])
+                    failures = int(row['failures'])
+                    errors = int(row['errors'])
+
                     if (repr(row['launchType']) == repr('Test')):
                         test_launches += 1
+                        if (successes > 0 and failures == 0 and errors == 0):
+                            green_zones += 1
                     elif (repr(row['launchType']) == repr('Normal')):
                         normal_launches += 1
 
@@ -63,7 +70,8 @@ def get_work_sessions(infile, outfile):
                     'editSizeStmts': edit_size_stmts,
                     'testEditSizeStmts': test_edit_size_stmts,
                     'editSizeMethods': edit_size_methods,
-                    'testEditSizeMethods': test_edit_size_methods
+                    'testEditSizeMethods': test_edit_size_methods,
+                    'greenZones': green_zones
                 }
                 writer.writerow(to_write)
 
@@ -72,9 +80,15 @@ def get_work_sessions(infile, outfile):
                 edit_size_methods = int(row['editSizeMethods'])
                 test_edit_size_methods = int(row['testEditSizeMethods'])
                 start_time = int(row['wsStartTime'])
+                successes = int(row['successes'])
+                failures = int(row['failures'])
+                errors = int(row['errors'])
+                green_zones = 0
                 if (repr(row['launchType']) == repr('Test')):
                     test_launches = 1
                     normal_launches = 0
+                    if (successes > 0 and failures == 0 and errors == 0):
+                        green_zones += 1
                 elif (repr(row['launchType']) == repr('Normal')):
                     normal_launches = 1
                     test_launches = 0
@@ -101,7 +115,8 @@ def get_work_sessions(infile, outfile):
             'editSizeStmts': edit_size_stmts,
             'testEditSizeStmts': test_edit_size_stmts,
             'editSizeMethods': edit_size_methods,
-            'testEditSizeMethods': test_edit_size_methods
+            'testEditSizeMethods': test_edit_size_methods,
+            'greenZones': green_zones
         }
         writer.writerow(to_write)
 
