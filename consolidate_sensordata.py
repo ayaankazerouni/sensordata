8dd444ddb3b83ef6bdca0c818e86a3d43637ab65
Ayaan Kazerouni <ayaan@vt.edu>
2017-10-30 13:12:40 -0400

    Consume method_metrics as part of larger consolidation.

--- consolidate_sensordata.py
+++ consolidate_sensordata.py
@@ -3,8 +3,11 @@
 import datetime
 import pandas as pd
 
+from method_metrics import get_method_metrics
+
 def consolidate_student_data(webcat_path=None, raw_inc_path=None,
-                             time_path=None, ref_test_gains_path=None, ws_path=None):
+                             time_path=None, ref_test_gains_path=None, ws_path=None,
+                             repo_mining_path=None):
     """
     Import, format, and consolidate incremental development metrics from several sources.
 
@@ -30,30 +33,31 @@ def consolidate_student_data(webcat_path=None, raw_inc_path=None,
     if ws_path is None:
         ws_path = 'data/fall-2016/work_sessions.csv'
 
-    webcat_data = load_webcat_submission_data(webcat_path) # get webcat submission data
+    if repo_mining_path is None:
+        repo_mining_path = 'data/fall-2016/repo-mining.csv'
+
+    webcat_data = __load_webcat_submission_data(webcat_path) # get webcat submission data
     ref_test_gains = __load_ref_test_data(ref_test_gains_path) # get ref-test-gains data and format it
     raw_inc_data = __load_raw_inc_data(raw_inc_path) # get raw incremental programming data and format it
-    time_data = load_time_spent_data(time_path) # get time spent on projects
+    time_data = __load_time_spent_data(time_path) # get time spent on projects
     launch_totals = __load_launch_totals(ws_path) # get launch totals from work session data
+    method_metrics = get_method_metrics(repo_mining_path) # get aggregated repo-mining metrics
 
     merged = webcat_data.merge(right=ref_test_gains, left_index=True, right_index=True) \
         .merge(right=raw_inc_data, left_index=True, right_index=True) \
         .merge(right=time_data, left_index=True, right_index=True) \
-        .merge(right=launch_totals, left_index=True, right_index=True)
-
-    hours_from_deadline = (merged['dueDateRaw'] - merged['submissionTimeRaw'])
-    merged['finished.hours.from.deadline'] = hours_from_deadline.apply(lambda diff: diff.total_seconds() / 3600)
-    merged['on.time.submission'] = merged['finished.hours.from.deadline'].apply(lambda h: 1 if h >= 0 else 0)
+        .merge(right=launch_totals, left_index=True, right_index=True) \
+        .merge(right=method_metrics, left_index=True, right_index=True)
 
     days_from_deadline = (merged['dueDateRaw'] - merged['projectStartTime'])
-    merged['started.days.from.deadline'] = days_from_deadline.apply(lambda diff: diff.days)
+    merged['startedDaysFromDeadline'] = days_from_deadline.apply(lambda diff: diff.days)
 
     # drop identifying column
     merged = merged.reset_index().set_index(['userId', 'assignment']).drop('userName', axis=1)
 
     return merged
 
-def load_webcat_submission_data(webcat_path):
+def __load_webcat_submission_data(webcat_path):
     """
     Loads webcat submission data from webcat_path. Only returns the student's final submission
     on each projects. Submission data is modified so that score.correctness
@@ -85,6 +89,12 @@ def load_webcat_submission_data(webcat_path):
     data['elementsCovered'] = (data['elementsCovered'] / data['elements']) / 0.98
     data['elementsCovered'] = data['elementsCovered'].apply(lambda x: x if x <= 1 else 1)
     data['score.reftest'] = data['score.correctness'] / data['elementsCovered']
+
+    #  calculate submission time outcomes
+    hours_from_deadline = (data['dueDateRaw'] - data['submissionTimeRaw'])
+    data['finishedHoursFromDeadline'] = hours_from_deadline.apply(lambda diff: diff.total_seconds() / 3600)
+    data['onTimeSubmission'] = data['finishedHoursFromDeadline'].apply(lambda h: 1 if h >= 0 else 0)
+
     data.set_index(['userName', 'assignment'], inplace=True)
     return data
 
@@ -136,7 +146,7 @@ def __load_raw_inc_data(raw_inc_path):
 
     return data
 
-def load_time_spent_data(time_path):
+def __load_time_spent_data(time_path):
     cols_of_interest = [
         'email',
         'assignment',
